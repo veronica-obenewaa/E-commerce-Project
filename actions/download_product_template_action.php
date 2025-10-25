df1acb413b1f7ab78f3af69041af62aade368662
veronicaobenewaa <obenewaaveronica2@gmail.com>
2025-10-25 18:31:50 +0000

    new

--- actions/download_product_template_action.php
+++ actions/download_product_template_action.php
@@ -6,6 +6,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+var_dump($cats);
+var_dump($brands);
+exit;
+
+
 
 require_once __DIR__ . '/../settings/core.php';
 require_once __DIR__ . '/../controllers/category_controller.php';
